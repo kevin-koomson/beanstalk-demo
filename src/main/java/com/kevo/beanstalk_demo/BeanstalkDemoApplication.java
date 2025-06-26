4473afd099c7438392d78ce416a93cd68092158a
Kevin Koomson <andrew.koomson@amalitech.com>
2025-06-26 11:06:19 +0000

    feat: app init

---
+++ src/main/java/com/kevo/beanstalk_demo/BeanstalkDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kevo.beanstalk_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanstalkDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanstalkDemoApplication.class, args);
+	}
+
+}
